9986dd59858b5e8683b1bbfd9d15476fb83bc4cf
Daniele <100372697+daniele-segreto@users.noreply.github.com>
2024-04-05 16:38:12 +0200

    iniettato HrrpClient e realizzata la funzione register() in AuthService

--- frontend/src/app/auth.service.ts
+++ frontend/src/app/auth.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  register(userData: any) {
+    return this.http.post<any>('http://your-api-domain.com/api/register', userData);
+  }
 }
