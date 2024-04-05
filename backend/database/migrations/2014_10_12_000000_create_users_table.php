c560244335971d8089e6118dd7d3fcdfce61f65d
Daniele <100372697+daniele-segreto@users.noreply.github.com>
2024-04-05 16:29:06 +0200

    migrazione tabella users

--- backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('confirmation_code')->nullable();
+            $table->boolean('confirmed')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
