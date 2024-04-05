a25be470f173fe7ae2a45f10885612c592ac2ea8
Daniele <100372697+daniele-segreto@users.noreply.github.com>
2024-04-05 16:32:48 +0200

    implementazione del metodo register() su AuthController

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -3,8 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        $confirmation_code = str_random(30);
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+            'confirmation_code' => $confirmation_code,
+        ]);
+
+        $user->sendEmailVerificationNotification();
+
+        return response()->json(['message' => 'User registered successfully. Please verify your email.'], 200);
+    }
 }
